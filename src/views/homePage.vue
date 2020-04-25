2c8157cabea4cf1267304cf7c8062448a2c053a5
yangbaiwan <wx_b3a1132fce1f40eaa058aabea23eaae9@git.code.tencent.com>
2020-04-25 20:35:43 +0800

    tianjia

---
+++ src/views/homePage.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="homePage">
+    <div class="header-title">
+      <div class="header-title-banner">
+        <img src="@/assets/img/logo_03.png" alt="" />
+        <img src="@/assets/img/logo_05.png" alt="" />
+      </div>
+    </div>
+    <div class="header-title-bg">
+      <div class="header-nav-box">
+        <div v-for="item in navList" :key="item">
+          {{ item }}
+        </div>
+      </div>
+    </div>
+    <div class="main">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      navList: [
+        '首页',
+        '品牌介绍',
+        '分龄课程',
+        '课程优势',
+        '教学模式',
+        '授课名师',
+        '精彩图集',
+        '原创文章',
+        '联系我们',
+      ],
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+.homePage {
+  .header-title {
+    // position: relative;
+    overflow: auto;
+    background-color: rgb(47, 172, 228);
+    padding-top: 10px;
+    .header-title-banner {
+      width: 1200px;
+      height: 74px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0 auto;
+    }
+    .header-title-nav {
+      position: absolute;
+      left: 50%;
+      width: 1200px;
+      margin-left: -600px;
+      margin: 0 auto;
+      /deep/.el-menu {
+        display: flex;
+        /deep/.el-menu-item {
+          font-size: 16px;
+          flex: 1;
+          text-align: center;
+        }
+      }
+    }
+  }
+  .header-title-bg {
+    background-image: url(../assets/img/png_18.png);
+    height: 65px;
+    position: absolute;
+    z-index: 3;
+    width: 100%;
+    .header-nav-box {
+      width: 1200px;
+      margin: 0 auto;
+      display: flex;
+      height: 43px;
+      div {
+        flex: 1;
+        text-align: center;
+        line-height: 43px;
+        color: #ffffff;
+        cursor: pointer;
+      }
+    }
+  }
+  .main {
+    // margin-top: -20px;
+    z-index: 2;
+  }
+}
+</style>
---
+++ src/views/shouye.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="onePage">
+    <div class="onePage-nav">
+      <el-carousel height="544px" indicator-position="none">
+        <el-carousel-item v-for="item in navImgList" :key="item">
+          <img :src="item" alt="" />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="onePage-title">
+      <div
+        v-for="(item, index) in onePageTitle"
+        :key="index"
+        :style="{ background: item.color }"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+    <div class="onePage-describe">
+      <div class="onePage-describe-box">
+        <img src="../assets/img/banner_11.png" alt="" />
+        <div>
+          <div style="font-size:30px;">英语小神童，让英语启蒙更简单</div>
+          <div style="width:100%; margin:0 auto; height:30px;"></div>
+          <div style="font-size:18px;  line-height:38px;">
+            国内首创的“英语启蒙成长树”体系<br />
+            0-6岁宝宝的“分龄分阶”递进式课程<br />
+            科学的体系、趣味的儿歌、好玩的游戏、原版的绘本、丰富的动画<br />
+            每天15分钟，鼓励他、引导他，陪他用英语去看世界！
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      navImgList: [require('../assets/img/2018227646517659.png')],
+      onePageTitle: {
+        one: {
+          title: '国内首创的“英语启蒙成长树”体系',
+          color: 'rgb(254, 194, 7)',
+        },
+        twe: {
+          title: '专注0-6岁婴幼儿英语在家启蒙课程',
+          color: 'rgb(252, 109, 114)',
+        },
+        three: {
+          title: '享誉全球的优质教学资源,用爱启蒙',
+          color: 'rgb(32, 213, 196)',
+        },
+      },
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+.el-carousel__item img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.onePage-title {
+  position: absolute;
+  left: 50%;
+  width: 1200px;
+  height: 67px;
+  margin: -30px auto 0;
+  margin-left: -600px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 11;
+  div {
+    flex: 1;
+    height: 100%;
+    line-height: 67px;
+    text-align: center;
+    color: #ffffff;
+    font-size: 20px;
+  }
+}
+.onePage-describe {
+  width: 100%;
+  height: 382px;
+  background: url(../assets/img/banner_08.png) center right no-repeat;
+  background-color: #f4e8d8;
+  .onePage-describe-box {
+    width: 1200px;
+    margin: 0 auto;
+    height: 242px;
+    padding-top: 70px;
+    display: flex;
+    img {
+      width: 235px;
+      height: 242px;
+    }
+    div {
+      flex: 1;
+      text-align: center;
+    }
+  }
+}
+</style>
